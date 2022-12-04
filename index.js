fe9766e3992f7e33764a23e148abfdc8265f61f2
dopezinho <opugnero@gmail.com>
2022-12-04 10:21:07 -0300

    condicionais 3

--- index.js
+++ index.js
@@ -22,10 +22,24 @@ if (dinheiro > 50){
 
 // CONDICIONAIS 2
 
+/*
 var selecao = 'Brasil'
 
 if (selecao == 'Brasil' || selecao == 'Argentina' || selecao == 'Uruguai'){
     console.log(`${selecao} ja ganho doido`)
 } else {
     console.log(`${selecao} nunca ganho essa parada`)
+}
+*/
+
+//CONDICIONAIS 3
+
+var selecao = 'manadiocarolapombarolingope'
+
+if (selecao == 'Brasil') {
+    console.log(`${selecao} ja ganho 5 veis doido`)
+} else if (selecao == 'Argentina' || selecao == 'Uruguai') {
+    console.log(`${selecao} ganho 2 veis só troxa`)
+} else {
+    console.log(`ixii ${selecao} nunca ganho bb`)
 }
